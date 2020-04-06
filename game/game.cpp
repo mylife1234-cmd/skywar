3d63f25cbb5b4e0f705b9689f65a9582cecb7783
mylife1234-cmd <mdc011645996@gmail.com>
2020-04-06 22:33:35 +0700

    Third commit

--- game/game.cpp
+++ game/game.cpp
@@ -2,44 +2,72 @@
 #include<SDL.h>
 #include<SDL_image.h>
 using namespace std;
-const int SCREEN_WIDTH=640;
-const int SCREEN_HEIGHT=480;
+const int SCREEN_WIDTH=1200;
+const int SCREEN_HEIGHT=600;
 const string WINDOW_TITLE = "SKY WAR";
 bool Init();
 bool LoadMedia();
 void close();
 void waitUntilKeyPressed();
+SDL_Surface *LoadSurface(std::string path);
 SDL_Window *window=NULL;
 SDL_Surface *screenSurface=NULL;
 SDL_Surface *LoadImage=NULL;
 int main(int argc, char *argv[])
 {
     //Start up SDL and create window
-    if(!Init())
-    {
-        cout << "Failed to initialize";
-    }
-    else{
-        if(!LoadMedia())
-        {
-            cout << "Failed to load Image";
-        }
-        else{
-            //Apply the image
-            SDL_BlitSurface(LoadImage,NULL,screenSurface,NULL);
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-            waitUntilKeyPressed();
-        }
-    }
-    close();
+    if( !Init() )
+	{
+		cout << "Failed to initialize!\n" ;
+	}
+	else
+	{
+		//Load media
+		if( !LoadMedia() )
+		{
+			cout << "Failed to initialize!\n" ;
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+
+				}
+                SDL_Rect strectchRect;
+                strectchRect.x=0;
+                strectchRect.y=0;
+                strectchRect.w=SCREEN_WIDTH;
+                strectchRect.h=SCREEN_HEIGHT;
+                SDL_BlitScaled(LoadImage,NULL,screenSurface, &strectchRect);
+				//Update the surface
+				SDL_UpdateWindowSurface( window );
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
     return 0;
 }
 
 bool Init()
 {
     bool success=true;
-    if(SDL_Init(SDL_INIT_VIDEO)<0)
+    if(SDL_Init(SDL_INIT_EVERYTHING)==-1)
     {
         cout << "SDL could not initialize! SDL_Error:" << SDL_GetError();
         success=false;
@@ -53,8 +81,16 @@ bool Init()
             success=false;
         }
         else{
-            //Create Surface
-            screenSurface=SDL_GetWindowSurface(window);
+            //Initialize PNG loading
+            int imgFlags=IMG_INIT_PNG;
+            if(!(IMG_Init(imgFlags)& imgFlags) )
+            {
+                cout << "SDL_image could not initialize" << IMG_GetError();
+                success=false;
+            }
+            else{
+                screenSurface=SDL_GetWindowSurface(window);
+            }
         }
     }
     return success;
@@ -62,7 +98,7 @@ bool Init()
 bool LoadMedia()
 {
     bool success=true;
-    LoadImage=IMG_Load("background.png");
+    LoadImage=LoadSurface("bg2.png");
     if(LoadImage==NULL)
     {
         cout << "Unable to load image" << SDL_GetError();
@@ -79,6 +115,24 @@ void close()
     SDL_DestroyWindow(window);
     window=NULL;
     SDL_Quit();
+}
+SDL_Surface *LoadSurface(std::string path)
+{
+    SDL_Surface *optimizedSurface=NULL;
+    SDL_Surface *loadedSurface=IMG_Load(path.c_str());
+    if(loadedSurface==NULL)
+    {
+        cout << "Unable to load image" << path.c_str() << SDL_GetError();
+    }
+    else{
+        optimizedSurface=SDL_ConvertSurface(loadedSurface,screenSurface->format,0);
+        if(optimizedSurface==NULL)
+        {
+            cout << "Unable to load optimized image" << SDL_GetError();
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    return optimizedSurface;
 }
 void waitUntilKeyPressed()
 {
